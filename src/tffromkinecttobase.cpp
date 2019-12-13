b3fee206f0609ea231396df1443bdb1a660c60cf
fulva1230 <b10503137@gapps.ntust.edu.tw>
2019-12-13 14:21:50 +0800

    complete the mapping system

--- src/tffromkinecttobase.cpp
+++ src/tffromkinecttobase.cpp
@@ -15,11 +15,13 @@ int main(int argc, char **argv) {
     ros::Rate r(1);
 
     tf::TransformBroadcaster broadcaster;
-
+    tf::Quaternion quaternion{};
+    quaternion.setRPY(0, 0, M_1_PI / 2);
     while (n.ok()) {
         broadcaster.sendTransform(
+
                 tf::StampedTransform(
-                        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0, OFFSET_Y, HEIGHT)),
+                        tf::Transform(quaternion, tf::Vector3(0, OFFSET_Y, HEIGHT)),
                         ros::Time::now(), "base_link", "camera_depth_frame"));
         r.sleep();
     }
